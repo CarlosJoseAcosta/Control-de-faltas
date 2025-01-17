b08b7ad65018582aa667c41a9cf6501032849ec0
CarlosJoseAcosta <carlosacostaxd1@gmail.com>
2025-01-17 18:12:38 +0000

    problema con el seeder solucionado

--- controlDeAusencias/database/migrations/0001_01_01_000001_create_users_table.php
+++ controlDeAusencias/database/migrations/0001_01_01_000001_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -22,6 +23,10 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        Artisan::call('db:seed', [
+            '--class' => 'UserSeeder',
+        ]);
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
