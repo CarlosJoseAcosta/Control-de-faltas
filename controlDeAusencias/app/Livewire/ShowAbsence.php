1dfe790f02b2a54e481c0801f92c1341924f3217
CarlosJoseAcosta <116382349+CarlosJoseAcosta@users.noreply.github.com>
2025-01-24 23:53:41 +0000

    comienzo de la muestra segun la hora actual

--- controlDeAusencias/app/Livewire/ShowAbsence.php
+++ controlDeAusencias/app/Livewire/ShowAbsence.php
@@ -26,7 +26,26 @@ class ShowAbsence extends Component
         $mes = date("m");
         $anio = date("Y");
         $fechaAct = $anio."-".$mes."-".$dia;
+        $arayHoras = array(
+            "1º mañana" => "8:55",
+            "2º mañana" => "9:50",
+            "3º mañana" => "10:45",
+            "recreo mañana" => "11:15",
+            "4º mañana" => "12:10",
+            "5º mañana" => "13:05",
+            "6º mañana" => "14:00",
+            "1º tarde" => "14:55",
+            "2º tarde" => "15:50",
+            "3º tarde" => "16:45",
+            "recreo tarde" => "17:15",
+            "4º tarde" => "18:10",
+            "5º tarde" => "19:05",
+            "6º tarde" => "20:00",
+        );
+        /*falta añadir el array exclusivo para el martes*/
+        /*select que muestra todas las faltas de la fecha actual*/
         // $this->ausencias = Absence::select('users.name as profesor','absences.time as hora','departments.name as departamento','absences.comment as comentario')->join('users','users.id','=','absences.user_id')->join('departments','departments.id','=','users.department_id')->where('absences.created_at','like',$fechaAct)->get();
+        /*select que muestra todas las faltas*/
         $this->ausencias = Absence::select('users.name as profesor','absences.time as hora','departments.name as departamento','absences.comment as comentario')->join('users','users.id','=','absences.user_id')->join('departments','departments.id','=','users.department_id')->get();
         $this->todosDep = Department::select('name')->where('id','!=','1')->get();
     }
